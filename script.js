c12d0b4419ba3b78ae837bd937b742671e097e23
Daniel Hug <daniel@hugwebdesign.com>
2015-09-09 18:11:07 -0700

    add index.html and script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+var me = {
+	name: { 
+		first: "Manuela",
+		last: "velasco"
+	},
+	gender: 'female',
+	age: 23,
+	married: true,
+	children: []
+};
+
+var brother1 = {
+	name: { first: 'Gene', last: 'Peterson' },
+	gender: 'male',
+	age: 20,
+	married: false,
+	children: []
+};
+
+var dad = {
+	name: { first: 'Peter', last: 'Peterson' },
+	gender: 'male',
+	age: 45,
+	married: true,
+	children: [me, brother1]
+};
